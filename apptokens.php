7ff2435a1a7a1deafecda6ec96f85256a468fbf5
Vasandkumar <kvasandkumar@gmail.com>
2014-08-20 00:40:57 +0530

    Token Error Check

--- apptokens.php
+++ apptokens.php
@@ -1,13 +1,10 @@
 <?php
 
-//$consumer_key='iuzzTqlSR0Uk9uelZj4zVg';
-//$consumer_secret='vtjTM5Z3b6FwIxsFNqk9n8IBqGNdzAYFmasZOppKaLk';
-//$user_token='1220704718-TaqDyYoU6ibkNZJ4YsTLYoodSUDDlmmpBwiJqJO';
-//$user_secret='MjCb6zXKgTh7QIR4LSkVrxLIPDxcU0LIU5aLlyDwFs';
 
-$consumer_key='wCplLS0VNiiiEzVtNHIfGw';
-$consumer_secret='1JlnV2d55MdfEiiIlYEAf8IzIe2dp4Onk3WjNisE9s';
-$user_token='1220704718-7RNit9H4jTpJrcaPyiliwqUeXnXr0zoifnlFLrh';
-$user_secret='t0YTw4T1MsXK232XGfNOeEeDHnspycwVrXmgdYo';
+
+$consumer_key='';
+$consumer_secret='';
+$user_token='';
+$user_secret='';
 
 ?>
